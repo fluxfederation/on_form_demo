f3b11cc1d3800636563d6c8d6defa495fc4d6d59
Will Bryant <will.bryant@gmail.com>
2016-10-04 13:08:10 +1300

    use take_identity_from to DRY form tags

--- app/forms/edit_customer_form.rb
+++ app/forms/edit_customer_form.rb
@@ -1,4 +1,6 @@
 class EditCustomerForm < OnForm::Form
+  take_identity_from :customer
+
   expose %i(name email phone_number), on: :customer
 
   validates_presence_of :email
--- app/forms/new_customer_form.rb
+++ app/forms/new_customer_form.rb
@@ -1,4 +1,6 @@
 class NewCustomerForm < OnForm::Form
+  take_identity_from :customer
+
   expose %i(name email phone_number), on: :customer
 
   validates_presence_of :email, :phone_number
